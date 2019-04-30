e39178f9c4c981d893c70b6ffdf91bd43fd35946
macle <ysys86a@gmail.com>
2019-04-30 23:18:25 +0900

    candle update

--- src/main/java/com/seomse/trading/technical/analysis/candle/CandleManager.java
+++ src/main/java/com/seomse/trading/technical/analysis/candle/CandleManager.java
@@ -1,11 +1,19 @@
 package com.seomse.trading.technical.analysis.candle;
+
+import com.seomse.trading.technical.analysis.candle.candles.TradeCandles;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * <pre>
  *  파 일 명 : CandleManager.java
  *  설    명 : 캔들 관리자
  *
  *  작 성 자 : macle
- *  작 성 일 : 2019.03
+ *  작 성 일 : 2019.04
  *  버    전 : 1.0
  *  수정이력 :
  *  기타사항 :
@@ -14,12 +22,10 @@ package com.seomse.trading.technical.analysis.candle;
  */
 public class CandleManager {
 
+    private static final Logger logger = LoggerFactory.getLogger(CandleManager.class);
 
 
-
-
-
-
+    private Map<Long, TradeCandles> candleMap = new HashMap<>();
 
     /**
      * 생성자
@@ -27,6 +33,27 @@ public class CandleManager {
     public CandleManager(){
 
     }
+
+
+    /**
+     * 캔들추가
+     * @param time 캔들 기준시간
+     * @return 추가 성공 여부
+     */
+    public boolean addCandle(long time){
+
+
+
+
+        return false;
+    }
+
+
+
+
+
+
+
 
 
 }
