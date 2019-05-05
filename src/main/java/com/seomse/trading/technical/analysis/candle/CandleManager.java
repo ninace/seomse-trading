a8633081cb45f2b79ed80dbd84e3c41761b4301e
macle <ysys86a@gmail.com>
2019-05-05 21:05:49 +0900

    candle update

--- src/main/java/com/seomse/trading/technical/analysis/candle/CandleManager.java
+++ src/main/java/com/seomse/trading/technical/analysis/candle/CandleManager.java
@@ -1,5 +1,6 @@
 package com.seomse.trading.technical.analysis.candle;
 
+import com.seomse.trading.Trade;
 import com.seomse.trading.technical.analysis.candle.candles.TradeCandles;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,7 +14,7 @@ import java.util.Map;
  *  설    명 : 캔들 관리자
  *
  *  작 성 자 : macle
- *  작 성 일 : 2019.04
+ *  작 성 일 : 2019.05
  *  버    전 : 1.0
  *  수정이력 :
  *  기타사항 :
@@ -24,36 +25,52 @@ public class CandleManager {
 
     private static final Logger logger = LoggerFactory.getLogger(CandleManager.class);
 
-
     private Map<Long, TradeCandles> candleMap = new HashMap<>();
 
+    private TradeCandles [] tradeCandlesArray;
     /**
      * 생성자
+     * @param trades 거래정보 배열
+     * @param candleTimes 캔들 생성을위한 캔들 생성 기준 타임 배열
      */
-    public CandleManager(){
+    public CandleManager(Trade [] trades, long [] candleTimes){
 
+
+        TradeCandles [] tradeCandlesArray = new TradeCandles[candleTimes.length];
+
+        for (int i = 0; i <candleTimes.length ; i++) {
+            tradeCandlesArray[i] = new TradeCandles(candleTimes[i]);
+            candleMap.put(candleTimes[i], tradeCandlesArray[i]);
+        }
+
+        this.tradeCandlesArray = tradeCandlesArray;
+
+        // 순서정보가 명확해야 할 경우를 위한 for
+        //noinspection ForLoopReplaceableByForEach
+        for (int i = 0; i <trades.length ; i++) {
+            addTrade(trades[i]);
+        }
+        logger.debug("candle manager create complete: " + trades.length + ", " + candleTimes.length);
     }
 
 
     /**
-     * 캔들추가
-     * @param time 캔들 기준시간
-     * @return 추가 성공 여부
+     * 거래정보 추가
+     * @param trade 거래 정보
      */
-    public boolean addCandle(long time){
-
-
-
-
-        return false;
+    public void addTrade(Trade trade){
+        for(TradeCandles tradeCandles : tradeCandlesArray){
+            tradeCandles.addTrade(trade);
+        }
     }
 
-
-
-
-
-
-
-
+    /**
+     * 캔들배열관리정보 얻기
+     * @param time 기준시간
+     * @return 기준시간에 맞는 캔들배열관리정보
+     */
+    public TradeCandles getCandles(long time){
+        return candleMap.get(time);
+    }
 
 }
