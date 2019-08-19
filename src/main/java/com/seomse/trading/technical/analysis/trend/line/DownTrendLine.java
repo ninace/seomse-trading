858366aa5747babb9345cc0fe9405a3b03583bd7
macle <ysys86a@gmail.com>
2019-08-19 20:29:22 +0900

    작업정리중

--- src/main/java/com/seomse/trading/technical/analysis/trend/line/DownTrendLine.java
+++ src/main/java/com/seomse/trading/technical/analysis/trend/line/DownTrendLine.java
@@ -1,4 +1,7 @@
 package com.seomse.trading.technical.analysis.trend.line;
+
+import com.seomse.trading.technical.analysis.candle.TradeCandle;
+
 /**
  * <pre>
  *  파 일 명 : DownTrendLine.java
@@ -14,4 +17,33 @@ package com.seomse.trading.technical.analysis.trend.line;
  * @author Copyrights 2019 by ㈜섬세한사람들. All right reserved.
  */
 public class DownTrendLine {
+
+
+    public double tilt(TradeCandle[] candles, int index, int leftCount ){
+//        int minusShortGapCount = 0;
+//        int minusCount =0;
+//        for(int i=startIndex ; i<index ; i++){
+//
+//            if(candles[i].getClose() > candles[i].getOpen()){
+//                continue;
+//            }
+//            minusCount++;
+//
+//            if(candles[i].getOpen() - candles[i].getOpen()*shortGapPercent > candles[i].getClose()){
+//                //종가가 shortGap보다 하락률이 클경우
+//                minusShortGapCount++;
+//            }
+//        }
+//
+//        if(minusCount < 4 || minusShortGapCount < 3) {
+//            //음봉 건수 유효성성
+//            return -1.0;
+//        }
+//
+//        double score = upperTail/(absChange*2.0);
+//        return score;
+
+        return 0.0;
+    }
+
 }
