21c218f7b580a795edaf1e8879f9d9c3d2fff8ce
macle <ysys86a@gmail.com>
2019-08-20 21:44:21 +0900

    기본적분선 재무 제표

---
+++ src/main/java/com/seomse/trading/fundamental/analysis/FinancialStatements.java
@@ -0,0 +1,23 @@
+package com.seomse.trading.fundamental.analysis;
+
+
+/**
+ * <pre>
+ *  파 일 명 : FinancialStatements.java
+ *  설    명 : 재무 제표
+ *
+ *  작 성 자 : macle
+ *  작 성 일 : 2019.08.20
+ *  버    전 : 1.0
+ *  수정이력 :
+ *  기타사항 :
+ * </pre>
+ * @author Copyrights 2019 by ㈜섬세한사람들. All right reserved.
+ */
+public class FinancialStatements {
+    //추정여부 (예상 재무 제표)
+    private boolean isEstimation = false;
+
+
+
+}
